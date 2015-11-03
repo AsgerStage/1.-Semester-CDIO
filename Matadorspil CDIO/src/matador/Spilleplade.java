e49fb680825b9fd2e23069301ec506e70f464af3
Asger <Asger@192.168.1.38>
2015-11-03 16:16:16 +0100

    Felter tilføjet

--- Matadorspil CDIO/src/matador/Spilleplade.java
+++ Matadorspil CDIO/src/matador/Spilleplade.java
@@ -1,9 +1,27 @@
 package matador;
 
-public class Spilleplade {
+import java.util.ArrayList;
 
-	public Spilleplade() {
-		// TODO Auto-generated constructor stub
+public class Spilleplade
+{
+
+	ArrayList<Felt> felter = new ArrayList<Felt>();
+	ArrayList<Spiller> spillere = new ArrayList<Spiller>();
+	
+	public Spilleplade() 
+	{
+		felter.add(new Betalingsfelt("Tower",250));
+		felter.add(new Betalingsfelt("Crater",-100));
+		felter.add(new Betalingsfelt("Palace gates",100));
+		felter.add(new Betalingsfelt("Cold Desert",-20));
+		felter.add(new Betalingsfelt("Walled city",180));
+		felter.add(new Betalingsfelt("Monastery",0));
+		felter.add(new Betalingsfelt("Black cave",-70));
+		felter.add(new Betalingsfelt("Huts in the mountain",60));
+		felter.add(new Betalingsfelt("The Werewall",-80));
+		felter.add(new Betalingsfelt("The pit",-50));
+		felter.add(new Betalingsfelt("Goldmine",650));
+		
 	}
 
 }
