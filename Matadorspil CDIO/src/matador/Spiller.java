c483b84e252eabedbfb8df92f883631926226aa5
Asger <Asger@192.168.1.38>
2015-11-05 13:48:51 +0100

    Tilføjelse af getKonto() metode til test

--- Matadorspil CDIO/src/matador/Spiller.java
+++ Matadorspil CDIO/src/matador/Spiller.java
@@ -1,26 +1,23 @@
 package matador;
 
-public class Spiller 
-{
+public class Spiller {
 
-		String navn;
-		double konto;
-		public int slag;
-		int position;
-		
-		public Spiller(String navn, double konto)
-		{
-			this.navn=navn;
-			this.konto=konto;
-			position=0;
-		}
-	
-		public void besked(String besked)
-		{
-			System.out.println(navn+": "+besked);
-		}
-		
-		public void transaktion(double kr)
+	String navn;
+	double konto;
+	public int slag;
+	int position;
+
+	public Spiller(String navn, double konto) {
+		this.navn = navn;
+		this.konto = konto;
+		position = 0;
+	}
+
+	public void besked(String besked) {
+		System.out.println(navn + ": " + besked);
+	}
+
+	public void transaktion(double kr)
 		{
 			konto = konto + kr;
 			System.out.println(navn+" har nu "+konto+" kr på sin konto.");
@@ -28,7 +25,11 @@ public class Spiller
 				System.out.println(navn+" har 3000 kroner og har vundet!!!");
 				System.exit(0); 
 			}
+		}
 		
-}
+	public double getKonto()
+			{
+				return konto;
+			}
 }
 
