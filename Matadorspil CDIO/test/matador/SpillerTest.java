4c6bb4679515672d002f93c6ee6b3cc7adee90e9
Asger <Asger@192.168.1.38>
2015-11-05 13:58:11 +0100

    Test af transaktionsmetode

--- Matadorspil CDIO/test/matador/SpillerTest.java
+++ Matadorspil CDIO/test/matador/SpillerTest.java
@@ -9,11 +9,14 @@ public class SpillerTest {
 	Spiller Asger = new Spiller("Asger", 1000);
 	int konto =1000;
 	String navn= "testnavn";
-	int kr = 500;
+	
 	@Test
 	public void testTransaktion()
 	{
-		System.out.println(Asger.getKonto());
+		
 		assertEquals(1000,Asger.getKonto(),0);
+		Asger.transaktion(500);
+		assertEquals(1500,Asger.getKonto(),0);
+		
 }
-	}
+}
