b864661ebc6fb11c5eb0792f81ebaa29988f4ca1
Asger <Asger@192.168.1.38>
2015-11-27 19:52:24 +0100

    GUI tilføjet

--- Matadorspil CDIO/src/matador/Spilleplade.java
+++ Matadorspil CDIO/src/matador/Spilleplade.java
@@ -13,24 +13,25 @@ public class Spilleplade
 	
 	public Spilleplade() 
 	{
-		/*felter.add(new Territory("Tribe Encampment",2500,500));
-		felter.add(new Territory("Crater",2500,500));
-		felter.add(new Territory("Cold Desert",2500,500));
-		felter.add(new Territory("Black Cave",2500,500));
-		felter.add(new Territory("The Werewall",2500,500));
-		felter.add(new Territory("Mountain Village",2500,500));
-		felter.add(new Territory("South Citadel",2500,500));
-		felter.add(new Territory("Palace gates",2500,500));
-		felter.add(new Territory("Tower",2500,500));
-		felter.add(new Territory("Castle",2500,500));
+		felter.add(new Territory("Tribe Encampment",1000,100));
+		felter.add(new Territory("Crater",1500,300));
+		felter.add(new Territory("Mountain",2000,500));
+		felter.add(new Territory("Cold Desert",3000,700));
+		felter.add(new Territory("Black Cave",4000,1000));
+		felter.add(new Territory("The Werewall",4300,1300));
+		felter.add(new Territory("Mountain Village",4750,1600));
+		felter.add(new Territory("South Citadel",5000,2000));
+		felter.add(new Territory("Palace gates",5500,2600));
+		felter.add(new Territory("Tower",2500,6000));
+		felter.add(new Territory("Castle",2500,8000));
 		felter.add(new Refuge("Walled city",5000));
-		felter.add(new Refuge("Monastery",500));*/
+		felter.add(new Refuge("Monastery",500));
 		felter.add(new Laborcamp("Huts in the mountain",2500));
-		felter.add(new Laborcamp("The pit",2500));/*
-		felter.add(new Fleet("Second Sail",2500));
-		felter.add(new Fleet("Sea Grover",2500));
-		felter.add(new Fleet("The Buccaneers",2500));
-		felter.add(new Fleet("Privateer armade",2500));*/
+		felter.add(new Laborcamp("The pit",2500));
+		felter.add(new Fleet("Second Sail",4000));
+		felter.add(new Fleet("Sea Grover",4000));
+		felter.add(new Fleet("The Buccaneers",4000));
+		felter.add(new Fleet("Privateer armade",4000));
 		
 		
 		
