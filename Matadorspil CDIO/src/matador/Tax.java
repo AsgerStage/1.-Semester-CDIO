02bac75815d3aaf1ea70ef20f054ccdc6b5d90d1
Peterpc <Peterpc@Peter>
2015-11-27 18:51:18 +0100

    Tilføjet Tax class

---
+++ Matadorspil CDIO/src/matador/Tax.java
@@ -0,0 +1,41 @@
+package matador;
+
+public class Tax extends Felt {
+ 
+		String navn;
+		double tax;
+
+		
+		public Tax (String navn, double tax) //konstruktør til oprettelsen af betalingsfelter
+		{
+			this.tax=tax;
+			this.navn=navn;
+		}
+		
+
+
+		public void landet(Spiller spiller)                   
+		{
+			spiller.besked("Du er landet på "+navn);
+			{ if (spiller.spørgsmål("Tryk 'Ja' for at betale 4000kr - Ellers betaler du 10% af din nuværende formue"))
+			{
+				spiller.transaktion(-tax);
+			}
+			else
+			{
+				spiller.transaktion(spiller.getKonto()*0.9);
+			}
+			}
+		}
+		
+		
+		public void passeret(Spiller spiller)
+		{
+
+			spiller.besked("Passerer "+navn);
+		}
+	}
+
+
+
+
