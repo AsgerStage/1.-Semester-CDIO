a24fa2106e1618c1f9cb2f7ae55ed4ff639aa29c
Asger <Asger@192.168.1.38>
2015-11-27 18:31:12 +0100

    Fleet klasse tilføjet

---
+++ Matadorspil CDIO/src/matador/Fleet.java
@@ -0,0 +1,68 @@
+package matador;
+
+public class Fleet extends Felt
+{
+	
+	Spiller ejer;
+	double pris;
+	String navn;
+	double leje;
+
+	
+	public Fleet(String navn,double pris) //konstruktør til oprettelsen af betalingsfelter
+	{
+		this.pris =pris;
+		this.navn=navn;
+		
+		
+	}
+	
+
+
+	public void landet(Spiller spiller)                   
+	{
+		spiller.besked("Du er landet på "+navn);
+		if (spiller==ejer)
+		{
+			spiller.besked("Du ejer dette Territory");
+		}
+		else if (ejer==null)
+		{ if (spiller.spørgsmål("Vil du købe "+navn+" for "+pris+"?"))
+		{
+			spiller.transaktion(-pris);
+			ejer=spiller;
+			spiller.fleets= spiller.fleets+1;
+		}
+		}
+		else if (spiller.fleets==1)
+		{
+			spiller.besked("du er landet på en anden spillers grund, som ejer en fleet!");
+			spiller.betal(ejer, 500);
+		}
+		else if (spiller.fleets==2)
+		{
+			spiller.besked("du er landet på en anden spillers grund, som ejer 2 fleets!");
+			spiller.betal(ejer, 1000);
+		}
+		else if (spiller.fleets==3)
+		{
+			spiller.besked("du er landet på en anden spillers grund, som ejer 3 fleets!");
+			spiller.betal(ejer, 2000);
+		}
+		else if (spiller.fleets==4)
+		{
+			spiller.besked("du er landet på en anden spillers grund, som ejer 4 fleets!");
+			spiller.betal(ejer, 4000);
+		}
+		else {
+			spiller.besked("Noget er gået helt galt råb om hjælp");
+		}
+	}
+	
+	
+	public void passeret(Spiller spiller)
+	{
+
+		spiller.besked("Passerer "+navn);
+	}
+}
